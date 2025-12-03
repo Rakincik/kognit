827e4687c15751ae0dc96cc7bb846ed1c9cf2ff3
Rüstem Akıncık <r.akincikk@gmail.com>
2025-12-03 18:00:47 +0300

    fix Modal props typing

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,4 +1,15 @@
-export default function Modal({ data, close }) {
+type ModalData = {
+  title: string;
+  desc: string;
+  details: string[];
+};
+
+type ModalProps = {
+  data: ModalData;
+  close: () => void;
+};
+
+export default function Modal({ data, close }: ModalProps) {
   const whatsapp =
     "https://wa.me/905555555555?text=Kognita%20programı%20hakkında%20bilgi%20almak%20istiyorum.";
 
